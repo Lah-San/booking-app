05805d244a07aa4a33ec4414358c18ab4fb3a1f0
lahirusandun14@gmail.com <lahirusandun14@gmail.com>
2023-04-15 09:59:37 +0530

    booking-app code using MERN stack

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -4,7 +4,7 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function Image({ src, ...rest }) {
   src = src && src.includes
-    ("https://") ? src : "https://raw.githubusercontent.com/Lah-San/booking-app/main/api/uploads/" + src;
+    ("https://") ? src : "https://raw.githubusercontent.com/Lah-San/booking-app/main/api/" + src;
 
   return <LazyLoadImage {...rest} src={src} alt={""} effect="blur"/>;
 }
