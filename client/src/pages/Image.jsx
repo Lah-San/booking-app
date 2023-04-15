39c4bf7c35daa7e0e381b46dba1f4902121715db
lahirusandun14@gmail.com <lahirusandun14@gmail.com>
2023-04-15 14:03:15 +0530

    vercel

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function Image({ src, ...rest }) {
   src = src && src.includes
-    ("https://") ? src : "https://raw.githubusercontent.com/Lah-San/booking-app/main/api/" + src;
+    ("https://") ? src : "http://localhost:4000/" + src;
 
-  return <LazyLoadImage {...rest} src={src} alt={""} effect="blur"/>;
+  return <img {...rest} src={src} alt={""}/>;
 }
